1c9810c26e0387d1c80c42394306d82ad9baae4c
Graham Bates <info@grahambates.com>
2017-09-29 15:38:05 +0100

    Add basic installer

---
+++ bootstrap.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+ln -s nvim ~/.config/nvim
+ln -s omf ~/.config/omf
+ln -s spacemacs ~/.spacemacs
+ln -s tmux ~/.tmux
+ln -s gitignore ~/.gitignore
+ln -s gitignore ~/.gitingore
+ln -s gitconfig ~/.gitconfig
+ln -s tern-config ~/.tern-config
+ln -s tmux-powerlinerc ~/.tmux-powerline
+ln -s tmux.conf ~/.tmux.conf
+ln -s vimrc ~/.vimrc
+ln -s vim ~/.vim
