3c5ba24295f925f5505bcb626d41e57b564a9aa9
Graham Bates <info@grahambates.com>
2022-04-29 17:20:40 +0100

    remove unused stuff

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,13 +2,10 @@
 git submodule update --init --recursive
 
 ln -s ~/dotfiles/nvim/ ~/.config/nvim
-ln -s ~/dotfiles/omf/ ~/.config/omf
-ln -s ~/dotfiles/spacemacs ~/.spacemacs
 ln -s ~/dotfiles/tmux ~/.tmux
 ln -s ~/dotfiles/gitignore ~/.gitignore
 ln -s ~/dotfiles/gitignore ~/.gitingore
 ln -s ~/dotfiles/gitconfig ~/.gitconfig
-ln -s ~/dotfiles/tern-config ~/.tern-config
 ln -s ~/dotfiles/tmux-powerlinerc ~/.tmux-powerline
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/vimrc ~/.vimrc
